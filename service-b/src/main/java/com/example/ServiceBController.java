67693f469fd7becfebc354a156b94f0cf48b5252
jeacd <joao.euller@academico.ifpb.edu.br>
2025-09-01 19:43:22 -0300

    first commit

---
+++ service-b/src/main/java/com/example/ServiceBController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service-b")
+public class ServiceBController {
+
+    @GetMapping("/hello")
+    public String helloServiceB() {
+        return "Olá do Service B!";
+    }
+}
